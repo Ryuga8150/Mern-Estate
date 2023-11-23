abae158b96eb30f55f4710e3745e316e8fab52c9
Paras Rawat <drago138150@gmail.com>
2023-11-23 21:22:30 +0530

    Database connected

--- api/app.js
+++ api/app.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
-app.listen(300, () => {
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
   console.log("Server is listening");
 });
